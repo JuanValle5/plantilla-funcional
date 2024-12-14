04e5b134835abbbe30816fc92db254033f9b571e
TatiRengifo <yulieth.rengifo@correounivalle.edu.co>
2024-12-13 22:37:45 -0500

    PDF

---
+++ app/bin/main/taller/MultiplicarMatrices.scala
@@ -0,0 +1,33 @@
+package taller
+
+import util.Random
+
+class MultiplicarMatrices {
+//Creacion de variables y metodos preliminares
+    //Crear el tipo Matriz
+    private type Matriz = Vector[Vector[Int]]
+
+    //Funcion para obtener una matriz al azar
+    def matrizAlAzar(long :Int, vals :Int): Matriz = {
+        val v = Vector.fill(long,long){Random.nextInt(vals)}
+        v
+    }
+    //Funcion de la operacion producto punto
+    def prodPunto(v1: Vector[Int], v2: Vector[Int]):Int ={
+        (v1 zip v2).map({case (i,j) => (i*j)}).sum
+    }
+    //Funcion de la Matriz Traspuesta
+    def transpuesta(m: Matriz): Matriz ={
+        val l = m.length
+        Vector.tabulate(l,l)((i,j)=>m(j)(i))
+    }
+//Implimentacion del codigo Taller 3
+    //Funcion para la multiplicacion de matrices
+    def multMatriz(m1: Matriz, m2: Matriz):Matriz = {
+        // Verifica que las dimensiones de las matrices sean compatibles
+        require(m1.head.length == m2.length, "El número de columnas de la primera matriz debe ser igual al número de filas de la segunda matriz.")
+        //vamos a multiplicar 2 matrices al azar
+        val m2t = transpuesta(m2)
+        Vector.tabulate(m1.length,m2t.length)((i,j)=>prodPunto(m1(i),m2t(j)))
+    }
+}
---
+++ app/bin/test/taller/MultiplicarMatricesTest.scala
@@ -0,0 +1,46 @@
+package taller
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class MultiplicarMatricesTest extends AnyFunSuite {
+  val objMultiMat = new MultiplicarMatrices() // Instancia del objeto que multiplica matrices
+
+  test("Multiplicar matrices secuencial 2x2 - Caso 1") {
+    val matriz1: Vector[Vector[Int]] = Vector(Vector(1, 2), Vector(3, 4))
+    val matriz2: Vector[Vector[Int]] = Vector(Vector(2, 0), Vector(1, 2))
+    val resultadoEsperado: Vector[Vector[Int]] = Vector(Vector(4, 4), Vector(10, 8))
+    assert(objMultiMat.multMatriz(matriz1, matriz2) === resultadoEsperado)
+  }
+
+  test("Multiplicar matrices secuencial 2x2 - Caso 2") {
+    val matriz1: Vector[Vector[Int]] = Vector(Vector(0, 1), Vector(2, 3))
+    val matriz2: Vector[Vector[Int]] = Vector(Vector(1, 0), Vector(0, 1))
+    val resultadoEsperado: Vector[Vector[Int]] = Vector(Vector(0, 1), Vector(2, 3))
+    assert(objMultiMat.multMatriz(matriz1, matriz2) === resultadoEsperado)
+  }
+
+  test("Multiplicar matrices secuencial 2x2 - Caso 3") {
+    val matriz1: Vector[Vector[Int]] = Vector(Vector(1, -1), Vector(-1, 1))
+    val matriz2: Vector[Vector[Int]] = Vector(Vector(2, 3), Vector(4, 5))
+    val resultadoEsperado: Vector[Vector[Int]] = Vector(Vector(-2, -2), Vector(2, 2))
+    assert(objMultiMat.multMatriz(matriz1, matriz2) === resultadoEsperado)
+  }
+
+  test("Multiplicar matrices secuencial 2x2 - Caso 4") {
+    val matriz1: Vector[Vector[Int]] = Vector(Vector(2, 0), Vector(0, 2))
+    val matriz2: Vector[Vector[Int]] = Vector(Vector(3, 4), Vector(5, 6))
+    val resultadoEsperado: Vector[Vector[Int]] = Vector(Vector(6, 8), Vector(10, 12))
+    assert(objMultiMat.multMatriz(matriz1, matriz2) === resultadoEsperado)
+    //print(objMultiMat.multMatriz(matriz1, matriz2))
+  }
+
+  test("Multiplicar matrices secuencial 2x2 - Caso 5") {
+    val matriz1: Vector[Vector[Int]] = Vector(Vector(1, 2), Vector(3, 4))
+    val matriz2: Vector[Vector[Int]] = Vector(Vector(0, 1), Vector(1, 0))
+    val resultadoEsperado: Vector[Vector[Int]] = Vector(Vector(2, 1), Vector(4, 3))
+    assert(objMultiMat.multMatriz(matriz1, matriz2) === resultadoEsperado)
+  }
+}
